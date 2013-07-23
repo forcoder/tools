f928da068174dff9abd577ccd26feea294414720
forcoder <root@ACE-TEST-408.(none)>
2013-07-23 20:18:34 +0800

    add yes/no check

--- shell/autossh.sh
+++ shell/autossh.sh
@@ -7,6 +7,7 @@ set PASSWORD "*******"
 spawn /usr/bin/ssh qiaozhou@$ip
 expect {
     timeout { send_user "Timeout."; exit 1 }
+    "*yes/no" { send "yes\r"; exp_continue }
     "*password:" { send "$PASSWORD\r"; exp_continue }
     "*~]$ " { send "sudo su admin\r"; exp_continue }
     "*Password:" { send "$PASSWORD\r"; exp_continue }
