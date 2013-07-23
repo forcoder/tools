dd37cffb32b3bbb33c3672571ca1f97dfc1b1af8
forcoder <root@ACE-TEST-408.(none)>
2013-07-23 20:17:24 +0800

    update autossh

--- shell/autossh.sh
+++ shell/autossh.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/expect -f
 
-set PASSWORD "password\r"
-set IP "IP"
+set timeout 15
+set ip [lindex $argv 0] 
+set PASSWORD "*******"
 
-spawn /usr/bin/ssh qiaozhou@$IP
-expect "*password:"
-send "$PASSWORD"
-expect "*~]$ "
-send "sudo su admin\r"
-expect "*Password:"
-send "$PASSWORD"
-expect "*]$ "
+spawn /usr/bin/ssh qiaozhou@$ip
+expect {
+    timeout { send_user "Timeout."; exit 1 }
+    "*password:" { send "$PASSWORD\r"; exp_continue }
+    "*~]$ " { send "sudo su admin\r"; exp_continue }
+    "*Password:" { send "$PASSWORD\r"; exp_continue }
+    "*]$ " { }
+}
+
 interact
